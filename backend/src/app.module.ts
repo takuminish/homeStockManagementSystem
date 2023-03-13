389ea1890bb47fa6d1e4502c3b70ac1ff096b759
takuminish <takuminv1997@gmail.com>
2023-03-14 00:35:41 +0900

    TypeORMを使用したpostgresへの接続の設定を記述

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [
+        ConfigModule.forRoot({
+          isGlobal: true,
+          envFilePath: ['../.env'],
+        }),
+      ],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        type: 'postgres',
+        host: 'localhost',
+        port: config.get<number>('POSTGRES_PORT'),
+        username: config.get<string>('POSTGRES_USER'),
+        password: config.get<string>('POSTGRES_PASSWORD'),
+        database: config.get<string>('POSTGRES_DATABASE'),
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })

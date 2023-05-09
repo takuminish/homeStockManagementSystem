2d87215e8c1a56b9acb049b150c339b8af6a7e84
takuminish <takuminv1997@gmail.com>
2023-05-09 23:15:05 +0900

    jestの設定

---
+++ frontend-regist-stock/src/App.test.tsx
@@ -0,0 +1,8 @@
+import App from "./App";
+import { render, screen } from '@testing-library/react';
+
+test('renders App component', () => {
+    render(<App />);
+
+    expect(screen.getByText('Vite + React')).toBeTruthy();
+})

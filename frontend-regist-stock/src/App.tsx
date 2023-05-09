014198f2f2382985efc2bf6979b79a6158474a8d
takuminish <takuminv1997@gmail.com>
2023-05-09 23:19:14 +0900

    muiのインストール

--- frontend-regist-stock/src/App.tsx
+++ frontend-regist-stock/src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from '/assets/react.svg'
 import viteLogo from '/assets/vite.svg'
 import './App.css'
+import { Button } from '@mui/material'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Button variant="contained">muiのテスト</Button>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
